eab94c76a8c3f31d10a21913978c2012f3d5cce2
nrmeenmohamed <nrmeenelaraby@gmail.com>
2024-10-03 14:12:24 +0300

    add all packages

---
+++ lib/core/managers/color_manager.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ColorManager {
+  static const Color secondary = Color(0xff1dd7df);
+  static const Color blue = Color(0xff1E95D4);
+  static const Color primary = Color(0xffb5e5e7);
+}
